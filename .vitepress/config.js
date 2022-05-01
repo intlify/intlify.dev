e03f9ae06fb0139660d5cee727f98137757862fa
kazuya kawaguchi <kawakazu80@gmail.com>
2022-05-01 15:40:10 +0900

    update sponsors

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -89,6 +89,11 @@ const config = {
           title: 'Localazy',
           link: 'https://localazy.com/blog/how-to-localize-vuejs-app-with-vue-i18n-and-localazy?utm_source=kazupon&utm_medium=banner&utm_campaign=sponsorships_kazupon&utm_content=logo',
           logo: '/sponsors/localazy.svg'
+        },
+        {
+          title: 'Crowdin',
+          link: 'https://crowdin.com/teams/engineering?utm_source=vue-i18n.intlify.dev&utm_medium=referral',
+          logo: '/sponsors/crowdin.png'
         }
       ],
       sliver: [
