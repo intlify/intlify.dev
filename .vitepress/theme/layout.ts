e3b4d89716378762996e56b0970b13589a564a96
kazuya kawaguchi <kawakazu80@gmail.com>
2023-10-02 11:46:02 +0900

    chore: update sponsor

--- .vitepress/theme/layout.ts
+++ .vitepress/theme/layout.ts
@@ -33,11 +33,6 @@ export default {
         logo: '/sponsors/nuxtlabs.svg'
       },
       {
-        title: 'RapidAPI',
-        link: 'https://rapidapi.com',
-        logo: '/sponsors/RapidAPI.png'
-      },
-      {
         title: 'Localazy',
         link: 'https://localazy.com/blog/how-to-localize-vuejs-app-with-vue-i18n-and-localazy?utm_source=kazupon&utm_medium=banner&utm_campaign=sponsorships_kazupon&utm_content=logo',
         logo: '/sponsors/localazy.svg'
